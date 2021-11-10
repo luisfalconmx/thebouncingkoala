59b9bbb449bb803a45655e0bfd0fb576da659c67
luisfalconmx <luisfalconmx@gmail.com>
2021-11-09 19:43:16 -0600

    test: setting two mocks for exclude static files and styles

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 const path = require('path')
 
 module.exports = {
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: [path.resolve(__dirname, 'src/setupTests.js')],
-  testEnvironment: 'jsdom'
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      path.resolve(__dirname, '__mocks__/fileMock.js'),
+    '\\.(css|pcss|sss)$': path.resolve(__dirname, '__mocks__/styleMock.js')
+  }
 }
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import './index.pcss'
+import Logo from '@images/tailwindcss.png'
+import './index.pcss'
 
 const Hero = (props) => {
   const { title, image, imageAlt } = props
@@ -8,6 +9,7 @@ const Hero = (props) => {
   return (
     <div className="hero">
       <img src={image} alt={imageAlt} />
+      <img src={Logo} alt="Tailwindcss Logo" />
       <h1 className="hero__title">{title}</h1>
     </div>
   )
