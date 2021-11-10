3c1b8e6b8e49be691e0dcc900ab88c10cf09ff40
luisfalconmx <luisfalconmx@gmail.com>
2021-11-09 21:05:10 -0600

    test: the firts snapshot created with react-test-renderer library

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       path.resolve(__dirname, '__mocks__/fileMock.js'),
     '\\.(css|pcss|sss)$': path.resolve(__dirname, '__mocks__/styleMock.js')
-  }
+  },
+  snapshotResolver: path.resolve(__dirname, 'src/snapshotResolver.js')
 }
--- src/components/Hero/index.test.js
+++ src/components/Hero/index.test.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from './index'
 import { mount } from 'enzyme'
+import { create } from 'react-test-renderer'
 
-describe('Testing Hero component', () => {
+describe('<Hero />', () => {
   test('Mount component', () => {
     const hero = mount(<Hero />)
     expect(hero).toHaveLength(1)
+  })
+  test('Check the UI', () => {
+    const hero = create(<Hero />)
+    expect(hero.toJSON()).toMatchSnapshot()
   })
 })
