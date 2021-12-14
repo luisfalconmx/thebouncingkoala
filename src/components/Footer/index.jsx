43b649386f67ebffc7357903a397ae93d0b21c99
luisfalconmx <luisfalconmx@gmail.com>
2021-12-13 21:33:25 -0600

    feat: footer component was created in its desktop version

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
+import './index.pcss'
+
+import LogoFacebook from '@images/logo-facebook.svg'
+import LogoTwitter from '@images/logo-twitter.svg'
+import LogoInstagram from '@images/logo-instagram.svg'
+import LogoGithub from '@images/logo-github.svg'
+
+const SocialIcons = [
+  {
+    icon: LogoFacebook,
+    link: 'https://www.facebook.com/'
+  },
+  {
+    icon: LogoTwitter,
+    link: 'https://www.twitter.com/'
+  },
+  {
+    icon: LogoInstagram,
+    link: 'https://www.instagram.com/'
+  },
+  {
+    icon: LogoGithub,
+    link: 'https://www.github.com/'
+  }
+]
 
 const Footer = () => {
   return (
-    <footer>
-      Footer
-      <ul>
-        <li>
-          <a href="#">Link 1</a>
-        </li>
+    <footer className="footer">
+      <strong className="footer__leyend">
+        © 2020 Dlex, Inc. All rights reserved
+      </strong>
+      <ul className="footer__list">
+        {SocialIcons.map((item) => (
+          <li className="footer__item" key={item.link}>
+            <a className="footer__link" href={item.link}>
+              <img src={item.icon} />
+            </a>
+          </li>
+        ))}
       </ul>
     </footer>
   )
