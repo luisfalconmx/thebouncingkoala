35f4be5bcda74c3b3dbce8556065de70caa5b7f6
luisfalconmx <luisfalconmx@gmail.com>
2021-12-20 15:38:14 -0600

    style: the Navbar component was adjusted to the preliminary mobile version

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -50,52 +50,54 @@ const Navbar = () => {
   })
 
   return (
-    <header className="Navbar">
-      <a className="Navbar__Hotlink" href="/">
-        <img className="Navbar__Logo" src={Logo} />
-      </a>
-      <nav className="Navbar__Nav">
-        <ul className="Navbar__List">
-          {links.map((link) => (
-            <li className="Navbar__Item" key={link.text}>
-              <a className="Navbar__Link" href={link.href}>
-                {link.text}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <Button />
-        <div className="Navbar__Actions">
-          <div className="Navbar__ActionLanguage">
-            <TranslateIcon
-              className="Navbar__Icon Navbar__Icon--Language"
-              onClick={showTooltip}
-            />
-            <div className={tooltipClasses}>
-              <button
-                className="Navbar__TooltipButton"
-                type="button"
-                onClick={changeLangEs}
-              >
-                Español
-              </button>
-              <button
-                className="Navbar__TooltipButton"
-                type="button"
-                onClick={changeLangEn}
-              >
-                English
-              </button>
+    <>
+      <header className="Navbar">
+        <a className="Navbar__Hotlink" href="/">
+          <img className="Navbar__Logo" src={Logo} />
+        </a>
+        <nav className="Navbar__Nav">
+          <ul className="Navbar__List">
+            {links.map((link) => (
+              <li className="Navbar__Item" key={link.text}>
+                <a className="Navbar__Link" href={link.href}>
+                  {link.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <Button className="Navbar__Button" />
+          <div className="Navbar__Actions">
+            <div className="Navbar__ActionLanguage">
+              <TranslateIcon
+                className="Navbar__Icon Navbar__Icon--Language"
+                onClick={showTooltip}
+              />
+              <div className={tooltipClasses}>
+                <button
+                  className="Navbar__TooltipButton"
+                  type="button"
+                  onClick={changeLangEs}
+                >
+                  Español
+                </button>
+                <button
+                  className="Navbar__TooltipButton"
+                  type="button"
+                  onClick={changeLangEn}
+                >
+                  English
+                </button>
+              </div>
             </div>
+            {darkmode ? (
+              <SunIcon className="Navbar__Icon" onClick={toggleDarkmode} />
+            ) : (
+              <MoonIcon className="Navbar__Icon" onClick={toggleDarkmode} />
+            )}
           </div>
-          {darkmode ? (
-            <SunIcon className="Navbar__Icon" onClick={toggleDarkmode} />
-          ) : (
-            <MoonIcon className="Navbar__Icon" onClick={toggleDarkmode} />
-          )}
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   )
 }
 
