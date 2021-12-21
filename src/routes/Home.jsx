d24c1988fa254637b4355b329e1f7686ead002b1
luisfalconmx <luisfalconmx@gmail.com>
2021-12-21 01:02:37 -0600

    style: the Hero component was adjusted to the mobile version

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import Hero from '@containers/Hero'
-import Services from '@containers/Services'
-import Team from '@containers/Team'
-import Featured from '@containers/Featured'
-import Pricing from '@containers/Pricing'
-import FeaturedBlog from '@containers/FeaturedBlog'
-import FAQ from '@containers/FAQ'
-import Newsletter from '@containers/Newsletter'
+// import Services from '@containers/Services'
+// import Team from '@containers/Team'
+// import Featured from '@containers/Featured'
+// import Pricing from '@containers/Pricing'
+// import FeaturedBlog from '@containers/FeaturedBlog'
+// import FAQ from '@containers/FAQ'
+// import Newsletter from '@containers/Newsletter'
 
 const Home = () => (
   <>
     <Hero />
-    <Services />
-    <Team />
-    <Featured />
-    <Pricing />
-    <FeaturedBlog />
-    <FAQ />
-    <Newsletter />
+    {/* <Services /> */}
+    {/* <Team /> */}
+    {/* <Featured /> */}
+    {/* <Pricing /> */}
+    {/* <FeaturedBlog /> */}
+    {/* <FAQ /> */}
+    {/* <Newsletter /> */}
   </>
 )
 
